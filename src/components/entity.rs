ccc20d082aacb9c70309d0b64cf024fa65324f95
Guakocius <engelhardt.alexander@mein.gmx>
2026-06-21 17:02:41 +0200

    added faulty test

--- src/components/entity.rs
+++ src/components/entity.rs
@@ -11,10 +11,24 @@ pub struct Name(pub String);
 pub struct GreetTimer(pub Timer);
 
 pub fn add_entities(mut cmds: Commands) {
-    cmds.spawn((Player, Name("Bob Testrop".to_string())));
+    cmds.spawn((Player, Name("Player".to_string())));
     ["Asmodeus", "Entity2", "Entity3", "Entity4"]
         .iter()
         .for_each(|a| {
             cmds.spawn((Entity, Name(a.to_string())));
         });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_entities() {
+        let mut app = App::new();
+        app.init_resource::<Time>();
+        app.add_sytems(Startup, add_entities);
+
+        let entities = app.world().get::<Entity>().
+    }
 }
