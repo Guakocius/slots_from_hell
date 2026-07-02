2adce27ebb267cbb6d2550d60f251b548eac987f
Guakocius <engelhardt.alexander@mein.gmx>
2026-07-02 23:18:00 +0200

    Added enemy movement functionality

--- src/components/enemy.rs
+++ src/components/enemy.rs
@@ -1,7 +1,8 @@
 //! This module defines core structures and setup behaviors for game entities.
 
-#[doc(inline)]
+use crate::{GameState, menu::MenuState};
 use bevy::{color::palettes::css::DARK_MAGENTA, prelude::*};
+use rand::{prelude::*, random};
 
 /// Component representing an enemy `Enemy`.
 ///
@@ -18,12 +19,27 @@ use bevy::{color::palettes::css::DARK_MAGENTA, prelude::*};
 #[derive(Component)]
 pub struct Enemy {
     name: Name,
+    speed: EnemySpeed,
     pos: Vec3,
 }
 
+#[derive(Resource, Debug, Clone)]
+pub struct EnemySpeed(pub f32);
+
 impl Enemy {
-    fn new(name: Name, pos: Vec3) -> Self {
-        Self { name, pos }
+    fn new(name: Name, speed: EnemySpeed, pos: Vec3) -> Self {
+        Self { name, speed, pos }
+    }
+}
+
+#[derive(Debug)]
+pub struct EnemyPlugin;
+
+impl Plugin for EnemyPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(EnemySpeed(300.0))
+            .add_systems(OnEnter(GameState::Playing), add_enemies)
+            .add_systems(FixedUpdate, enemy_movement);
     }
 }
 
@@ -77,27 +93,49 @@ pub fn add_enemies(
     [
         Enemy::new(
             Name("Asmodeus".to_string()),
+            EnemySpeed(300.0),
             Vec3::new(1024.0, -1024.0, 0.0),
         ),
         Enemy::new(
             Name("Beelzebub".to_string()),
+            EnemySpeed(300.0),
             Vec3::new(-1024.0, -1024.0, 0.0),
         ),
         Enemy::new(
             Name("Poltergeist".to_string()),
+            EnemySpeed(300.0),
             Vec3::new(1024.0, 1024.0, 0.0),
         ),
-        Enemy::new(Name("Lucifer".to_string()), Vec3::new(-1024.0, 0.0, 0.0)),
+        Enemy::new(
+            Name("Lucifer".to_string()),
+            EnemySpeed(300.0),
+            Vec3::new(-1024.0, 0.0, 0.0),
+        ),
     ]
     .iter()
     .for_each(|e| {
         cmds.spawn((
+            Enemy::new(e.name.clone(), e.speed.clone(), e.pos),
             e.name.clone(),
             Mesh2d(meshes.add(Circle::new(10.0))),
             MeshMaterial2d(materials.add(Color::from(DARK_MAGENTA))),
             Transform::from_translation(e.pos),
         ));
-    })
+    });
+    cmds.insert_resource(EnemySpeed(300.0));
+}
+
+pub fn enemy_movement(
+    mut enemies_query: Query<&mut Transform, With<Enemy>>,
+    speed: Res<EnemySpeed>,
+    time: Res<Time<Fixed>>,
+) {
+    for mut enemies in enemies_query {
+        let direction = Vec3::new(random::<f32>(), random::<f32>(), 0.0);
+
+        let move_delta = direction.normalize_or_zero() * speed.0 * time.delta_secs();
+        enemies.translation += move_delta;
+    }
 }
 
 #[cfg(test)]
