a8195ee624623b3f941165cc0309b6948c79c5ec
Guakocius <engelhardt.alexander@mein.gmx>
2026-07-17 22:54:33 +0200

    Added functionality to the poltergeist enemy to move through walls

--- src/components/enemy.rs
+++ src/components/enemy.rs
@@ -1,8 +1,7 @@
 //! This module defines core structures and setup behaviors for game enemies.
 
 use crate::{GameState, Player, Wall, check_collision};
-use bevy::{color::palettes::css::DARK_MAGENTA, prelude::*};
-use rand::random;
+use bevy::prelude::*;
 
 /// Component representing an enemy `Enemy`.
 ///
@@ -22,6 +21,7 @@ pub struct Enemy {
     speed: EnemySpeed,
     pos: Vec3,
     sprite_path: String,
+    can_move_through_walls: bool,
 }
 
 /// The `movement speed` of the `enemies`.
@@ -38,12 +38,19 @@ pub struct Enemy {
 pub struct EnemySpeed(pub f32);
 
 impl Enemy {
-    fn new(name: Name, speed: EnemySpeed, pos: Vec3, sprite_path: String) -> Self {
+    fn new(
+        name: Name,
+        speed: EnemySpeed,
+        pos: Vec3,
+        sprite_path: String,
+        can_move_through_walls: bool,
+    ) -> Self {
         Self {
             name,
             speed,
             pos,
             sprite_path,
+            can_move_through_walls,
         }
     }
 }
@@ -119,24 +126,28 @@ pub fn add_enemies(mut cmds: Commands, assets: Res<AssetServer>) {
             EnemySpeed(300.0),
             Vec3::new(1024.0, -1024.0, 0.0),
             String::from("img/asmodeus.png"),
+            false,
         ),
         Enemy::new(
             Name("Beelzebub".to_string()),
             EnemySpeed(300.0),
             Vec3::new(-1024.0, -1024.0, 0.0),
             String::from("img/beelzebub.png"),
+            false,
         ),
         Enemy::new(
             Name("Poltergeist".to_string()),
             EnemySpeed(300.0),
             Vec3::new(1024.0, 1024.0, 0.0),
             String::from("img/poltergeist.png"),
+            true,
         ),
         Enemy::new(
             Name("Lucifer".to_string()),
             EnemySpeed(300.0),
             Vec3::new(-1024.0, 0.0, 0.0),
             String::from("img/lucifer.png"),
+            false,
         ),
     ]
     .iter()
@@ -147,6 +158,7 @@ pub fn add_enemies(mut cmds: Commands, assets: Res<AssetServer>) {
                 e.speed.clone(),
                 e.pos,
                 e.sprite_path.to_string(),
+                e.can_move_through_walls,
             ),
             e.name.clone(),
             Sprite {
@@ -173,7 +185,7 @@ pub fn add_enemies(mut cmds: Commands, assets: Res<AssetServer>) {
 /// app.add_systems(Update, enemy_movement).update();
 /// ```
 pub fn enemy_movement(
-    mut enemies_query: Query<&mut Transform, With<Enemy>>,
+    mut enemies_query: Query<(&mut Transform, &mut Enemy)>,
     player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
     wall_query: Query<(&Transform, &Wall), Without<Enemy>>,
     speed: Res<EnemySpeed>,
@@ -184,7 +196,7 @@ pub fn enemy_movement(
     };
     let player_pos = player_tf.translation;
 
-    for mut enemy_tf in &mut enemies_query {
+    for (mut enemy_tf, enemy) in &mut enemies_query {
         let direction = (player_pos - enemy_tf.translation).normalize_or_zero();
 
         let move_delta = direction * speed.0 * time.delta_secs();
@@ -193,7 +205,8 @@ pub fn enemy_movement(
 
         let mut collision = false;
         for (wall_tf, wall) in &wall_query {
-            if check_collision(new_pos, enemy_size, wall_tf, wall) {
+            if check_collision(new_pos, enemy_size, wall_tf, wall) && !enemy.can_move_through_walls
+            {
                 collision = true;
                 break;
             }
