a8356eff0ea7ed3cfcfcab745641e9838b9e7d8a
Guakocius <engelhardt.alexander@mein.gmx>
2026-07-21 01:28:49 +0200

    Added documentation for macros.rs

--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -1,7 +1,12 @@
-use bevy::prelude::*;
+//! This module serves for bringing utility-enhancing macros.
 
-use crate::Room;
-
+/// Spawns text using the specified parameters.
+///
+/// # Examples
+///
+/// ```
+/// spawn_text!(cmds, "Hello World".into(), (top, 12), (bottom, 12));
+/// ```
 #[macro_export]
 macro_rules! spawn_text {
     ($cmds:expr,$(($t:expr,($pos1:ident,$x:expr),($pos2:ident,$y:expr)$(,$misc:expr),*)),*) => {
@@ -9,7 +14,7 @@ macro_rules! spawn_text {
             $cmds.spawn((
                 Text::new($t),
                 TextFont {
-                    font_size: FontSize::Px(15.0),
+                    font_size: FontSize::Px(30.0),
                     ..default()
                 },
                 Node {
@@ -24,6 +29,21 @@ macro_rules! spawn_text {
     };
 }
 
+/// Utility macro for mass-inserting all specified resources in a loop.
+///
+/// # Examples
+///
+/// ```
+/// #[derive(Resource)]
+/// struct Resource1;
+/// #[derive(Resource)]
+/// struct Resource2;
+/// #[derive(Resource)]
+/// struct Resource3;
+///
+/// insert_resources!(Resource1);
+/// insert_resources!(Resource1, Resource2, Resource3);
+/// ```
 #[macro_export]
 macro_rules! insert_resources {
     ($cmds:expr,$($res:expr),*) => {
@@ -33,6 +53,15 @@ macro_rules! insert_resources {
     };
 }
 
+/// Generate rooms with the specified arguments and creates a [`Vec`] out of this loop.
+///
+/// # Examples
+///
+/// ```
+/// use bevy::prelude::*;
+/// generate_rooms!(("Test Room", Vec3::new(0.0, 0.0, -200.0), "textures/map_texture_floor.png"));
+/// generate_rooms!(("Test Room 1", Vec3::new(0.0, 0.0, -200.0), "textures/map_texture_floor.png"), ("Test Room 2", Vec3::new(1024.0, 1024.0, -200.0), "textures/map_texture_floor.png"));
+/// ```
 #[macro_export]
 macro_rules! generate_rooms {
     ($(($name:expr,$coords:expr,$texture:expr)),*) => {
@@ -45,6 +74,16 @@ macro_rules! generate_rooms {
     }
 }
 
+/// Checks if the `Entity` (either [`Player`] or [`Enemy`]) collides with the coordinates of either
+/// a [`Room`] or, if a wall size is specified, a [`Wall`].
+///
+/// # Examples
+///
+/// ```
+/// use bevy::prelude::*;
+/// check_collision!(Vec2::new(0.0, 0.0), Vec2::new(20.0, 20.0), Vec2::new(1024.0, 1024.0));
+/// check_collision!(Vec2::new(0.0, 0.0), Vec2::new(20.0, 20.0), Vec2::new(1024.0, 1024.0), Vec2::new(128.0, 64.0));
+/// ```
 #[macro_export]
 macro_rules! check_collision {
     ($pos:expr,$sz:expr,$opos:expr) => {
